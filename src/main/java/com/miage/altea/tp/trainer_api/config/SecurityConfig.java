124d04aff987cf9b6296480c59199b522e0c3a81
Florian DIDIER <didiflorian@gmail.com>
2019-03-18 09:41:30 +0100

    Add files via upload

--- src/main/java/com/miage/altea/tp/trainer_api/config/SecurityConfig.java
+++ src/main/java/com/miage/altea/tp/trainer_api/config/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.miage.altea.tp.trainer_api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override

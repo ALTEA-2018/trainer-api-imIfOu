5a947d71a1ee0ea0c3e0ad6950b3875d638ad94f
Florian DIDIER <didiflorian@gmail.com>
2019-03-18 09:47:06 +0100

    Update TrainerApi.java

--- src/main/java/com/miage/altea/tp/trainer_api/TrainerApi.java
+++ src/main/java/com/miage/altea/tp/trainer_api/TrainerApi.java
@@ -11,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class TrainerApi {
 
     public static void main(String... args){
